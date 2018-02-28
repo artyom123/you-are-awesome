2222a1472c2b24e805fc960a7e9ecb663fe25cfc
artyom123 <artvirsh@mail.ru>
2018-03-01 00:14:20 +0300

    you-are-awesome decided

--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (a) => {a};
+const createNotEnumerableProperty = (a) => {
+    Object.defineProperty(Object.prototype, a,{
+          set: value=>String.prototype.propertyValue = value,
+          get: ()=>String.prototype.propertyValue,
+    });
+    
+    return a;
+};
+const createProtoMagicObject = () => {
+    function magicObjFun(){}
+    magicObjFun.__proto__ = magicObjFun.prototype;
+    
+    return magicObjFun;
+};
+let num = 0,
+numAsync = 0;
+const incrementor = () => {
+	num++;
+    
+    function incrementorPlus(){
+        num++;
+        return incrementorPlus;
+    }
+    incrementorPlus.toString = function(){
+        return num;
+    }
+    
+    return incrementorPlus;
+};
+const asyncIncrementor = () => {
+    numAsync++;
+	setTimeout(function(){}, 0);
+  
+	return numAsync;
+};
+function* createIncrementer(){
+    let index = 0;
+    while(true) 
+        yield ++index;
+}
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = (element) => {
+    let prom = new Promise((resolve, reject) => {
+        setTimeout(()=>resolve(element),1000);
+    })
+
+    return prom;
+};
+const getDeepPropertiesCount = (object) => JSON.stringify(object).match(/[0-9]{1,}/g).length;
+const createSerializedObject = () => {
+    let obj = null;
+    return null;
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => arr.sort((a, b)=>a.__proto__ - b.__proto__);
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
